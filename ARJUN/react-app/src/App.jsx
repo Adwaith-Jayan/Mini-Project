c37fd896afaf23c536cef3eeb301385635c0288d
arjunsabu300 <arjunsabumark32@gmail.com>
2025-02-10 00:20:30 +0530

    changes

--- ARJUN/react-app/src/App.jsx
+++ ARJUN/react-app/src/App.jsx
@@ -1,14 +1,16 @@
-// src/App.jsx
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./Homepage/Home"; 
 import Login from "./Loginpage/Login"; 
 import Stockstatusupdate from "./Stockstatusupdatepage/Stockstatusupdate";
-import StockDetails from "./Stockdetailspage/Stockdetails";
 import Stocktransfer from "./Stocktransfer/stocktransfer";
 import Stockverifications from "./Stockverification/Stockverification";
 import Stockwarranty from "./Stockwarranty/Stockwarranty";
-
+import Stockdetails from "./Stockdetailspage/Stockdetails";
+import Stockclears from "./Stockclearence/Stockclearence";
+import Register from "./Registerpage/Register";
+import Deleteacc from "./deleteaccount/Deleteaccount";
 
 
 
@@ -16,7 +18,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Stockverifications/>} />
+        <Route path="/" element={<Deleteacc/>} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
