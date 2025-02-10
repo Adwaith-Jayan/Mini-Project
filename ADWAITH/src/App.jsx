d7816bed45101c5712a22c4ff78d3adbb5e6170d
unknown <adwaithjayan1234@gmail.com>
2025-02-10 22:58:51 +0530

    added assign faculty & maintenence pages

---
+++ ADWAITH/src/App.jsx
@@ -0,0 +1,11 @@
+import MaintenanceList from "./MaintenanceList/MaintenanceList"
+import AssignFaculty from "./AssignFaculty/AssignFaculty"
+function App() {
+  return (
+    <div>
+      <AssignFaculty/>
+    </div>
+  )
+}
+
+export default App
