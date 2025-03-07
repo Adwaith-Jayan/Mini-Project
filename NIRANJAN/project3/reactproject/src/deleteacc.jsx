5f2f67177e61851c7ccc4a3ed6fb2220eeee21ca
unknown <adwaithjayan1234@gmail.com>
2025-03-07 23:17:27 +0530

    fixed decc acc

--- NIRANJAN/project3/reactproject/src/deleteacc.jsx
+++ NIRANJAN/project3/reactproject/src/deleteacc.jsx
@@ -10,12 +10,10 @@ function Deleteacc() {
     event.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/user/delete", {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email_id: email }),
+      const response = await fetch("http://localhost:5000/api/delete-account", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email }),
       });
 
       const data = await response.json();
